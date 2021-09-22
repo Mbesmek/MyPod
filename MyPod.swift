cbe9cdd8d45ee98145b9e933d18d44c39c6d06e1
T66537 <melihbugra.esmek@ibtech.com.tr>
2021-09-22 19:13:03 +0300

    added unit tes

---
+++ MyPod.swift
@@ -0,0 +1,21 @@
+//
+//  MyPod.swift
+//  MyPod
+//
+//  Created by Melih Bugra Esmek on 22.09.2021.
+//
+
+import Foundation
+
+class MyPod{
+    
+    let name = "Melih"
+    
+    func sum(x:Int,y:Int) -> Int {
+        return x + y
+    }
+    
+    func multiply(x:Int,y:Int) -> Int {
+        return x * y
+    }
+}
--- MyPodTests/MyPodTests.swift
+++ MyPodTests/MyPodTests.swift
@@ -10,24 +10,18 @@ import XCTest
 
 class MyPodTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var myPod : MyPod!
+    
+    override func setUp() {
+        myPod = MyPod()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testSum() {
+        XCTAssertEqual(myPod.sum(x:5, y:6), 11)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testMultiply() {
+        XCTAssertEqual(myPod.multiply(x: 5, y: 6), 30)
     }
 
 }
